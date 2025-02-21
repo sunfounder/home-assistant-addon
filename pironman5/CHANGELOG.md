ff7420aa414d2974f87b8ab2aba30ef1459d78a5
Cavon <cavonxx@gmail.com>
2025-02-21 14:32:44 +0800

    update to 1.2.3

--- pironman5/CHANGELOG.md
+++ pironman5/CHANGELOG.md
@@ -1,6 +1,9 @@
 # Changelog
 
-## [1.1.3] - 2025-2-10
+## [1.2.3] - 2025-2-18
 
-- Add GPU Temperature
-- Update UI
+- Fix stop on close
+
+## [1.2.2] - 2025-2-17
+
+- Fix for remote access
--- pironman5/run.sh
+++ pironman5/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/with-contenv /usr/bin/bashio
 
-/opt/pironman5/venv/bin/pironman5-service start
+# start pironman5 service
+/opt/pironman5/venv/bin/pironman5-service --config-path /data/config.json start
 
 # test loop
 # while true; do
